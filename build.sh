45d377d5b744c9f2763fe676399f344d93b85629
Eric Weise <ericdweise@gmail.com>
2020-12-28 22:16:52 -0800

    corrected references

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 pdflatex -halt-on-error presentation.tex
+
+pdflatex -halt-on-error report.tex
+biber report.bcf
+pdflatex -halt-on-error report.tex
+pdflatex -halt-on-error report.tex
